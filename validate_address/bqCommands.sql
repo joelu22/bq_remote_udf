2eed6ff8137d7d5c0d171aa450d2512da465abde
Joel Uthe <joeluthe@gmail.com>
2022-12-16 15:51:21 -0500

    Updated a few comments and SQL statements

--- validate_address/bqCommands.sql
+++ validate_address/bqCommands.sql
@@ -3,10 +3,13 @@ REMOTE WITH CONNECTION `gcp528a1.us-central1.remote_cloudfunc_conn`
 OPTIONS (endpoint = 'https://validate-address-xju5dymn7q-uc.a.run.app')
 ;
 
-select val, JSON_VALUE(remote_udf.validate_address(val, 'US').result.address.formattedAddress)
-from 
-UNNEST(['2600 Benjamin Franklin Pkwy, Philadelphia, PA 19130',
-     '2600 Ben Franklin, Philly, PA 19130',
-     '2600 Ben Franklin, Philadelphia, PA',
-     '2600 Benjamin Franklin, Philly'
-     ]) AS val
+SELECT
+  address,
+  JSON_VALUE(remote_udf.validate_address(address,'US').result.address.formattedAddress) as formattedAddress
+FROM
+  UNNEST([
+  '2600 Benjamin Franklin Pkwy, Philadelphia, PA 19130',
+  '2600 Ben Franklin, Philly, PA 19130',
+  '2600 Ben Franklin, Philadelphia, PA',
+  '2600 Benjamin Franklin, Philly'
+  ]) AS address
